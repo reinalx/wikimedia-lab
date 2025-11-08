32fff8b5f255f01c0aa1fb5d46cf11f12a973543
reinalx <andresrci@ext.inditex.com>
2025-11-08 20:06:00 +0100

    feat: Add RestAnalysisMapper and implement event analysis creation in controller

--- consumer-wikimedia/api-rest/src/main/java/com/learn/wikimedialab/apirest/controller/WikimediaAnalysisApiController.java
+++ consumer-wikimedia/api-rest/src/main/java/com/learn/wikimedialab/apirest/controller/WikimediaAnalysisApiController.java
@@ -2,6 +2,8 @@ package com.learn.wikimedialab.apirest.controller;
 
 import com.learn.wikimedialab.apigenerator.openapi.api.wikimedia.analysis.AnalysisApi;
 import com.learn.wikimedialab.apigenerator.openapi.api.wikimedia.analysis.model.CreateEventAnalysisRequestDTO;
+import com.learn.wikimedialab.apirest.mapper.RestAnalysisMapper;
+import com.learn.wikimedialab.domain.ports.in.services.AnalysisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class WikimediaAnalysisApiController implements AnalysisApi {
 
+  private final AnalysisService analysisService;
+
+  private final RestAnalysisMapper restAnalysisMapper;
+
+  /**
+   * Creates an event analysis.
+   *
+   * @param requestDto the create event analysis request DTO
+   * @return a ResponseEntity indicating the result of the operation
+   */
   @Override
   public ResponseEntity<Void> createEventAnalysis(
-      CreateEventAnalysisRequestDTO createEventAnalysisRequestDTO) {
-    return null;
+      CreateEventAnalysisRequestDTO requestDto) {
+    this.analysisService.analyzeEvent(
+        this.restAnalysisMapper.toEventAnalysis(requestDto)
+    );
+    return ResponseEntity.ok().build();
   }
 }
