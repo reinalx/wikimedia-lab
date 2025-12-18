69a760a30497241cc10286affe21af01aceb44a0
reinalx <andresrci@ext.inditex.com>
2025-12-18 19:36:01 +0100

    feat: add unit tests for KafkaEventConsumerAdapter and KafkaEventAnalysisPublisher

---
+++ consumer-wikimedia/infrastructure/wikimedia-kafka/src/test/java/com/learn/wikimedialab/kafka/adapters/in/KafkaEventConsumerAdapterTest.java
@@ -0,0 +1,41 @@
+package com.learn.wikimedialab.kafka.adapters.in;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.learn.wikimedialab.domain.entities.WikimediaEvent;
+import com.learn.wikimedialab.domain.ports.in.services.EventsService;
+import com.learn.wikimedialab.kafka.mappers.JsonToObjectMapper;
+import org.instancio.junit.InstancioExtension;
+import org.instancio.junit.InstancioSource;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith({MockitoExtension.class, InstancioExtension.class})
+class KafkaEventConsumerAdapterTest {
+
+  @InjectMocks
+  private KafkaEventConsumerAdapter kafkaEventConsumerAdapter;
+
+  @Mock
+  private EventsService eventsService;
+
+  @Mock
+  private JsonToObjectMapper jsonToObjectMapper;
+
+  @ParameterizedTest
+  @InstancioSource(samples = 1)
+  void givenEvent_whenConsumeEvent_thenProcessEvent(String event, WikimediaEvent wikimediaEvent) {
+    // When
+    when(this.jsonToObjectMapper.convertJsonStringToEvent(event))
+        .thenReturn(wikimediaEvent);
+
+    this.kafkaEventConsumerAdapter.consumer(event);
+
+    // Then
+    verify(this.eventsService).processEvent(wikimediaEvent);
+  }
+}
