1aadfd0ca310d2a54a6c6db90677459cd45065f0
reinalx <andresrci@ext.inditex.com>
2025-12-20 21:32:02 +0100

    feat: add Maven wrapper and initial Spring Boot application setup with Testcontainers configuration

---
+++ wikimedia-it/src/main/java/com/learn/wikimedia/WikimediaItApplication.java
@@ -0,0 +1,13 @@
+package com.learn.wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikimediaItApplication.class, args);
+    }
+
+}
