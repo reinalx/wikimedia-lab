f8ee1cb11c437a228e96d720f16d1c6271eddde0
reinalx <andresrci@ext.inditex.com>
2026-01-11 17:31:00 +0100

    feat: add integration tests for streaming processing and configure Testcontainers for Kafka

--- producer-wikimedia/boot/src/main/java/com/learn/wikimedialab/MainApplication.java
+++ producer-wikimedia/boot/src/main/java/com/learn/wikimedialab/MainApplication.java
@@ -2,7 +2,6 @@ package com.learn.wikimedialab;
 
 import com.learn.wikimedialab.domain.services.StreamingProcessingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,27 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @RequiredArgsConstructor
-public class MainApplication implements CommandLineRunner {
+public class MainApplication {
 
-  private final StreamingProcessingService streamingProcessingService;
+    private final StreamingProcessingService streamingProcessingService;
 
-  /**
-   * Main method to run the Spring Boot application.
-   *
-   * @param args command-line arguments
-   */
-  public static void main(String[] args) {
-    SpringApplication.run(MainApplication.class, args);
-  }
+    /**
+     * Main method to run the Spring Boot application.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 
-
-  /**
-   * Runs the application and starts the streaming processing service.
-   *
-   * @param args command-line arguments
-   */
-  @Override
-  public void run(String... args) {
-    this.streamingProcessingService.startStreaming();
-  }
 }
